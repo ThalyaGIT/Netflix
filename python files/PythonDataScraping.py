c78902cd27b61c2b898774c6e7e5bdb1c5e81178
ThalyaGIT <limmingshan@gmail.com>
2023-01-12 01:16:31 +0000

    save work

--- python files/PythonDataScraping.py
+++ python files/PythonDataScraping.py
@@ -2,73 +2,71 @@
 import pandas as pd
 import requests
 import urllib
-import pandas as pd
 from requests_html import HTML
 from requests_html import HTMLSession
-import requests
 import bs4
 
-# # Defining Functions
-# def get_source(url):
-#     """Return the source code for the provided URL. 
+# Defining Functions
+def get_source(url):
+    """Return the source code for the provided URL. 
+    Args: 
+        url (string): URL of the page to scrape.
+    Returns:
+        response (object): HTTP response object from requests_html. 
+    """
 
-#     Args: 
-#         url (string): URL of the page to scrape.
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        return response
 
-#     Returns:
-#         response (object): HTTP response object from requests_html. 
-#     """
+    except requests.exceptions.RequestException as e:
+        print(e)
 
-#     try:
-#         session = HTMLSession()
-#         response = session.get(url)
-#         return response
+def get_googleInfo(title):
 
-#     except requests.exceptions.RequestException as e:
-#         print(e)
+    scrapedData = []
 
-# def get_googleInfo(title):
+    query = urllib.parse.quote_plus(title)
+    response = get_source("https://www.google.co.uk/search?q=" + query)
 
-#     scrapedData = []
-
-#     query = urllib.parse.quote_plus(title)
-#     response = get_source("https://www.google.co.uk/search?q=" + query)
-
-#     soup = bs4.BeautifulSoup(response.text,"html.parser")
+    soup = bs4.BeautifulSoup(response.text,"html.parser")
                         
-#     rating_object=soup.find_all( "div", {"class":"a19vA"} )
-#     info_object=soup.find_all( "div", {"class":"rVusze"} )
-#     desc_object=soup.find_all( "div", {"class":"PZPZlf hb8SAc"} )
+    rating_object=soup.find_all( "div", {"class":"a19vA"} )
+    info_object=soup.find_all( "div", {"class":"rVusze"} )
+    desc_object=soup.find_all( "div", {"class":"PZPZlf hb8SAc"} )
 
-#     scrapedData.append('Title: ' +title)
+    scrapedData.append('Title: ' +title)
 
-#     for rating in rating_object:
-#         ratingInfo = rating.getText()
-#         rating = ratingInfo[0:3]
-#         scrapedData.append('Rating: ' +rating)
+    for rating in rating_object:
+        ratingInfo = rating.getText()
+        rating = ratingInfo[0:3]
+        scrapedData.append('Rating: ' +rating)
 
-#     for info in info_object:
-#         info = info.getText()
-#         if 'Language' in info:
-#             scrapedData.append(info)
-#             break
+    for info in info_object:
+        info = info.getText()
+        if 'Language' in info:
+            scrapedData.append(info)
+            break
     
-#     for info in info_object:
-#         info = info.getText()
-#         if 'Genre' in info:
-#             scrapedData.append(info)
-#             break
+    for info in info_object:
+        info = info.getText()
+        if 'Genre' in info:
+            scrapedData.append(info)
+            break
 
-#     for desc in desc_object:
-#         description = desc.getText()
-#         description = description[0:11] + str(': ') + description[11:]
-#         scrapedData.append(description)
-#         break
+    for desc in desc_object:
+        description = desc.getText()
+        description = description[0:11] + str(': ') + description[11:]
+        scrapedData.append(description)
+        break
     
-#     return scrapedData
+    return scrapedData
 
-# # Test Function
-# get_googleInfo("Squid Game")
+# Test Function
+print("hi")
+get_googleInfo("Squid Game")
+
 
 # # Apply data scraping function on all movie/show titles in dataset and save info into new CSV
 # data = pd.read_csv('netflix_titles.csv')
